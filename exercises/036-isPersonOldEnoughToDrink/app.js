38423c3523226bd07184692023d8636e93c39d94
franciscosvr37 <siglasconexion@gmail.com>
2025-09-29 23:42:00 +0000

    nuevo commit

--- exercises/036-isPersonOldEnoughToDrink/app.js
+++ exercises/036-isPersonOldEnoughToDrink/app.js
@@ -1,4 +1,8 @@
 function isPersonOldEnoughToDrink(person) {
   // Add your code after this line
-  
+ return person.age >= 21 
 }
+let obj = {
+  age: 16
+}
+console.log(isPersonOldEnoughToDrink(obj))
