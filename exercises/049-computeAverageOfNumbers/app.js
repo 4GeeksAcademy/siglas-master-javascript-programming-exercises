c774acb0343b2de480e25dc1584ea29b98c825ff
franciscosvr37 <siglasconexion@gmail.com>
2025-10-07 00:17:33 +0000

    otro comit

--- exercises/049-computeAverageOfNumbers/app.js
+++ exercises/049-computeAverageOfNumbers/app.js
@@ -1,1 +1,11 @@
 // Write your function here
+function computeAverageOfNumbers(input){
+    if (input.length==0) return 0
+    let total = 0
+    for(let item of input){
+        total += item
+    }
+    return total/input.length
+}
+let output = computeAverageOfNumbers([1,2,3,4,5])
+console.log(output)
